2e575d097a2442066157574bf086d9df83acbda2
Pepito-Manaloto <aaronjohn.asuncion@gmail.com>
2015-01-09 21:23:06 +0800

    Commit changes in home-pc: - version change in strings.xml - change db name from private to public in MySQLiteHelper.java

--- src/com/aaron/vocabulary/model/MySQLiteHelper.java
+++ src/com/aaron/vocabulary/model/MySQLiteHelper.java
@@ -11,7 +11,7 @@ import android.util.Log;
 public class MySQLiteHelper extends SQLiteOpenHelper
 {
     public static final String TAG = "MySQLiteHelper";
-    private static final String DATABASE_NAME = "aaron_vocabulary.db";
+    public static final String DATABASE_NAME = "aaron_vocabulary.db";
     private static final int DATABASE_VERSION = 1;
 
     public static final String TABLE_VOCABULARY = "vocabulary";
