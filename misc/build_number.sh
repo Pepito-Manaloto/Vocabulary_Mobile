b94f279555787ca274326ace253628a80852f308
Aaron John Asuncion <aaronjohn.asuncion@gmail.com>
2017-10-20 21:59:11 +0800

    Fixed build_number.sh

--- misc/build_number.sh
+++ misc/build_number.sh
@@ -5,10 +5,10 @@ count=$(git log --oneline | wc -l | gawk '{print $1}');
 revision=$(git rev-parse --short HEAD);
 
 dash=\&#8211\;
-version=$branch$dash$count$dash$revision;
+version=$branch\\$dash$count\\$dash$revision;
 
 echo "Version: $version";
 
 string_xml='../app/src/main/res/values/strings.xml';
 
-sed -i "s/<string name=\"build_num\">*<\/string>/<string name=\"build_num\">$version<\/string>/g" "$string_xml";
+sed -i "s/\(<string name=\"build_num\">\).*\(<\/string>\)/\1$version\2/g" "$string_xml";
