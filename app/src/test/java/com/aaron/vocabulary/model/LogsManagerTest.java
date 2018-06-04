8b5a1cd7a5ec32a49c2ad5ab76be59a270604571
Aaron John Asuncion <aaronjohn.asuncion@gmail.com>
2018-06-04 22:19:22 +0800

    Implemented CirclesCI. Added fullBackupContent=true in AndroidManifest.xml

--- app/src/test/java/com/aaron/vocabulary/model/LogsManagerTest.java
+++ app/src/test/java/com/aaron/vocabulary/model/LogsManagerTest.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
